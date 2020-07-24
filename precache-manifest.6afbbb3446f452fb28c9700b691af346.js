1de6ec9f145773d177b92282e60246bc8e2dbdd0
mateusz <osowski.mateuszz@gmail.com>
2020-07-24 20:04:16 +0200

    Updates

---
+++ precache-manifest.6afbbb3446f452fb28c9700b691af346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efce552fe6a1cf5acad95d57aa488a85",
+    "url": "/netfix-clone/index.html"
+  },
+  {
+    "revision": "ea2b87210a6d12f35e7e",
+    "url": "/netfix-clone/static/css/main.de015520.chunk.css"
+  },
+  {
+    "revision": "305c042a02dbd7c74993",
+    "url": "/netfix-clone/static/js/2.37164868.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netfix-clone/static/js/2.37164868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2b87210a6d12f35e7e",
+    "url": "/netfix-clone/static/js/main.0d13dea5.chunk.js"
+  },
+  {
+    "revision": "0902ca7ae88c4c3f213d",
+    "url": "/netfix-clone/static/js/runtime-main.0dd9a8e4.js"
+  }
+]);
